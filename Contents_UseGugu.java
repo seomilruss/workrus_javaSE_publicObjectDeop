bdb7045f6017ff146483b6f8a1077866f08ebd94
seomilruss <54846678+seomilruss@users.noreply.github.com>
2020-08-06 02:03:40 +0900

    Add files via upload

---
+++ Contents_UseGugu.java
@@ -0,0 +1,39 @@
+package publicobjectDeop.contents;
+
+class Gugu {
+	void partOutput() {
+		int dan = 1;
+		int doubleNum = 1;
+		while(doubleNum < 9) {
+			doubleNum++;
+			System.out.println(
+				dan + " * " + doubleNum + " = " + dan * doubleNum + "\t"
+			);
+		}
+	}
+	void allOutput() {
+		for(int dan=1; dan < 10; dan++) {
+			for(int doubleNum =1; doubleNum < 10; doubleNum++) {
+				System.out.print(
+					dan + " * " + doubleNum + " = " + dan * doubleNum + "\t"
+				);
+			}
+			System.out.println();
+		}
+	}
+}
+
+class GuguCore {
+	Gugu gugu;
+	public GuguCore() {
+		gugu = new Gugu();
+		gugu.allOutput();
+		gugu.partOutput();
+	}
+}
+
+class Contents_UseGugu {
+	public static void main(String[] args) {
+		new GuguCore();
+	}
+}
